3442f0e077e30bc34403e76ff15fe438145502a1
SaadkamalShaikhdev <saadkamaldev@gmail.com>
2026-07-25 13:26:31 +0500

    Added: vercel analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono,Poppins,Inter } from "next/font/google";
 import "./globals.css";
+import { Analytics } from "@vercel/analytics/next"
 
 
 
@@ -33,7 +34,7 @@ export default function RootLayout({
       <body
         className={`${inter.variable} ${poppins.variable} antialiased`}
       >
-         
+         <Analytics />
           {children}
           
       </body>
